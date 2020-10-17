232498e5a31bbba386d14636c419c156242e3eb4
Jorge Corzo <jorgecorzo@iMac-de-Jorge.local>
2020-10-16 22:19:01 -0500

    creacion proyecto

---
+++ app/Http/Controllers/StudentsController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Students;
+use Illuminate\Http\Request;
+
+class StudentsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students = Students::all();
+        return $students;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $student = Students::create($request->all());
+        return $student;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Students  $students
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Students $students)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Students  $students
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Students $students)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Students  $students
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Students $students)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Students  $students
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Students $students)
+    {
+        //
+    }
+}
